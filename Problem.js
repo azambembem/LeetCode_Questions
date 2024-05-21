70260fe4a5d04d35145d3607eb3cb689dd1d2f58
azambembem <azamjan980713@gmail.com>
2024-05-21 22:47:15 +0900

    first commit

---
+++ Problem.js
@@ -0,0 +1,17 @@
+// Ex1
+// function sumDigits(num) {
+//   let sum = 0;
+
+//   while (num > 0) {
+//     // num을 10으로 나눈 나머지를 sum에 더함
+//     sum += num % 10;
+//     // num을 10으로 나누어 다음 자릿수로 이동
+//     num = Math.floor(num / 10);
+//   }
+
+//   return sum;
+// }
+
+// console.log(sumDigits(123)); // 6
+// console.log(sumDigits(9045)); // 18
+// console.log(sumDigits(3)); // 3
